2a5c58bf1b4402b7be89be48af84fd24db44a078
Den <97998799+DNSAndrv@users.noreply.github.com>
2022-01-27 17:55:18 +0300

    Update phpdev-init.sh

--- phpdev-init.sh
+++ phpdev-init.sh
@@ -5,7 +5,7 @@ read -p 'Имя пользователя: '  userName
 apt -y install curl wget software-properties-common apt-transport-https git nano php php-fpm nginx mysql-server phpmyadmin  &&
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | apt-key add - &&
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" &&
-apt install code &&
+apt install code zsh &&
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &&
 mysql -u root --execute="
 UPDATE mysql.user SET plugin = 'mysql_native_password', authentication_string  = '' WHERE user = 'root';
@@ -16,7 +16,7 @@ CREATE DATABASE IF NOT EXISTS ${DbName};
 GRANT ALL PRIVILEGES ON ${DbName}.* TO '${userName}'@'localhost';
 " &&
 echo 'Change rootAllowNoPassord in phpmyadmin settings\n' &&
-echo '== Open nano ==' && sleep 1 &&
+echo '== Open nano ==' &&
 nano /etc/phpmyadmin/config.inc.php 
 
 chmod -R g+w /var/www && chown -R www-data:www-data /var/www &&
